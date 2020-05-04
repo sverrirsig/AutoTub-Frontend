d83f6a3e0b3b59b7887167b529b039dab3dbe28a
Sverrir Sigurðsson <sverrirs@netapp.com>
2020-05-04 10:52:37 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push git@github.com:sverrirsig/AutoTub-Frontend.git master:gh-pages
+git push -f git@github.com:sverrirsig/AutoTub-Frontend.git master:gh-pages
 
 cd -
