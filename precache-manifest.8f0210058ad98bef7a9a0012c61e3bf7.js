9c03d52074daffdc5a3ba938020c4b2d3358203f
Sverrir Sigurðsson <sverrirs@netapp.com>
2020-05-07 11:39:05 +0000

    deploy

---
+++ precache-manifest.8f0210058ad98bef7a9a0012c61e3bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8ae16c8a732b7b9286",
+    "url": "/AutoTub-Frontend/css/app.f6531f96.css"
+  },
+  {
+    "revision": "7686016fe8f178d300bb",
+    "url": "/AutoTub-Frontend/css/chunk-vendors.95ae28c2.css"
+  },
+  {
+    "revision": "27036cfa9844a2be6421a048e732dbcc",
+    "url": "/AutoTub-Frontend/index.html"
+  },
+  {
+    "revision": "9b8ae16c8a732b7b9286",
+    "url": "/AutoTub-Frontend/js/app.03c0e9da.js"
+  },
+  {
+    "revision": "7686016fe8f178d300bb",
+    "url": "/AutoTub-Frontend/js/chunk-vendors.f8a9fb57.js"
+  },
+  {
+    "revision": "196b38744eb81576d8fb32e598f1000e",
+    "url": "/AutoTub-Frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/AutoTub-Frontend/robots.txt"
+  }
+]);
